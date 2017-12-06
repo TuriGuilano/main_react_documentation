795c71fe86157058d09f5432414fd906b149e1b3
Janno Kapritsias <j.kapritsias@youngcapital.nl>
2017-12-06 11:23:46 +0100

    filestructure

---
+++ react-course-projects/indecision-app/src/components/Action.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Action = (props) => {
+  return (
+    <div>
+      <button
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+      >
+        Choose for me!
+      </button>
+    </div>
+  );
+};
+
+export default Action;
