64a645a93f098ddcf4c6ea80bb615be3211f8b66
Janno Kapritsias <j.kapritsias@youngcapital.nl>
2017-12-06 14:04:14 +0100

    finished webpack section

--- react-course-projects/indecision-app/src/components/AddOption.js
+++ react-course-projects/indecision-app/src/components/AddOption.js
@@ -2,21 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class AddOption extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined
-    };
-  }
-  handleAddOption(e) {
+  state = {
+    error: undefined
+  };
+  
+  handleAddOption = (e) => {
     e.preventDefault();
 
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
-
+    
     this.setState(() => ({ error }));
-
     // clear input if there was no error
     if (!error) {
       e.target.elements.option.value = '';
